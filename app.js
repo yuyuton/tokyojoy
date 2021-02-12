93316b1f3a06c7fc4cfda14fde8606a8f586dab4
Lingyu Feng <fenglingyu0819@gmail.com>
2021-02-13 00:17:42 +0900

    modify google oauth

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/tokyojoy",
+    callbackURL: absoluteURI + "/auth/google/tokyojoy",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
